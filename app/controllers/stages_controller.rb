1eabe9428cb3096f2bb2b2c4087693586313a644
billperegoy <bill@billperegoy.com>
2014-08-29 20:02:29 -0400

    Added basic new/craete classes and views for Stage.

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -1,2 +1,21 @@
 class StagesController < ApplicationController
+  def new
+    @project = Project.find(params[:project_id])
+    @stage = @project.stages.new
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @stage = @project.stages.new(stage_params)
+    if @stage.save
+      redirect_to :projects
+    else
+      render :stage
+    end
+  end
+
+  private
+  def stage_params
+    params.require(:stage).permit(:name, :level, :command)
+  end
 end
