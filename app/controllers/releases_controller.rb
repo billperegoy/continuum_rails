fd14c9ed039f55c392ee1c653962d94cada4a58b
billperegoy <bill@billperegoy.com>
2014-09-02 21:01:36 -0400

    Fixed nested resource link.

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -3,4 +3,9 @@ class ReleasesController < ApplicationController
     @project = Project.find(params[:project_id])
     @releases = @project.releases.all
   end
+
+  def show
+    @project = Project.find(params[:project_id])
+    @release = @project.releases.find(params[:id])
+  end
 end
