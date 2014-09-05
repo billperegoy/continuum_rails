ce358d70723ca3176fd64a11791be6e74d1a2d54
billperegoy <bill@billperegoy.com>
2014-09-04 21:47:17 -0400

    Added first bit of oauth code. Not sure why it's not doing the callback yet.

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,6 +7,7 @@ class Project < ActiveRecord::Base
   validates :path, format: { with: /\A\w+\/\w+\z/,
     message: 'must be <repos>/<proj>' }
   validate :path_must_exist_as_github_project
+  validate :can_create_github_hook
 
   def path_must_exist_as_github_project
     github = Github.new
@@ -17,10 +18,23 @@ class Project < ActiveRecord::Base
     end
   end
 
-  def repos_exist?(body, path)
-    body.each do |elem|
+  def repos_exist?(response_body, path)
+    response_body.each do |elem|
       return true if elem.full_name == path
     end
     return false
+  end
+
+  def can_create_github_hook
+    # This should create the hook and return true.
+    # If hook creation fails, return false.
+    #
+    client_id = '4614cd3accaefacf4694'
+    client_secret = '5dff49f531f6fecdaec3945ab7c5e94b115211b2'
+    callback_uri = 'http://sheep9.herokuapps.com/repositories/auth'
+
+    github = Github.new(client_id: client_id, client_secret: client_secret)
+    github.authorize_url(redirect_uri: callback_uri, scope: 'repo')
+    true
   end
 end
