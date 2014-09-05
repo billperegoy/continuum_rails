e357d8dc24271fe7edb594df6b89b0665073a3c1
billperegoy <bill@billperegoy.com>
2014-09-04 22:00:32 -0400

    Cleaned up oauth code.

--- app/models/project.rb
+++ app/models/project.rb
@@ -34,7 +34,12 @@ class Project < ActiveRecord::Base
     callback_uri = 'http://sheep9.herokuapps.com/repositories/auth'
 
     github = Github.new(client_id: client_id, client_secret: client_secret)
-    github.authorize_url(redirect_uri: callback_uri, scope: 'repo')
-    true
+    #github.authorize_url(redirect_uri: callback_uri, scope: 'repo')
+    github.authorize_url(scope: 'repo')
+
+    found_errors = false
+    if found_errors 
+      errors.add(:path, 'Could not authorize with github')
+    end
   end
 end
