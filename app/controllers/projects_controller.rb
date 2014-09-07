dd8193fad45b662c624d6b7717758f8315a5e5de
billperegoy <bill@billperegoy.com>
2014-09-07 16:09:20 -0400

    Oauth

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -69,8 +69,8 @@ class ProjectsController < ApplicationController
     github = Github.new(client_id: CLIENT_ID, client_secret: CLIENT_SECRET)
     @code = params[:code].to_s
     @token = github.get_token(@code).token
-    #response =  RestClient.get("https://api.github.com/repos/billperegoy/BowlingGame/hooks?access_token=#{@token}")
     response =  RestClient.get("https://api.github.com/users/billperegoy/repos?access_token=#{@token}")
+    response =  RestClient.get("https://api.github.com/repos/billperegoy/test/hooks?access_token=#{@token}")
     @data = JSON.parse(response)
     #@projects = Project.all
     #format.js {}
