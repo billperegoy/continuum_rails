9bfad2fabf2b3b659e1b39b1bfb309165576a596
billperegoy <bill@billperegoy.com>
2014-09-08 21:22:39 -0400

    Fixed validation scope in Project.

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -1,7 +1,12 @@
 class Stage < ActiveRecord::Base
   belongs_to :project
 
-  validates :name, presence: true, uniqueness: true
-  validates :level, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: {scope: :project_id}
+  validates :level, presence: true, uniqueness: {scope: :project_id}
   validates :command, presence: true
+  validate :level_is_valid
+
+  def level_is_valid
+    true
+  end
 end
