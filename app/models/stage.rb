c61418a48d108bed3ce16872d6558a93293d5602
billperegoy <bill@billperegoy.com>
2014-09-08 20:59:49 -0400

    Added initial validation for Stage.

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -1,3 +1,7 @@
 class Stage < ActiveRecord::Base
   belongs_to :project
+
+  validates :name, presence: true, uniqueness: true
+  #validates :level, presence: true, uniqueness: true
+  #validates :command, presence: true
 end
